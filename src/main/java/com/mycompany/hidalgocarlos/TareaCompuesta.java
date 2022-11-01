84312f13b53f869c8011311e5795479f9d4cf30d
sala8 <sala8@is-ls08e18.FI-LabInf.ucatolica.loc>
2022-11-01 13:40:25 -0500

    Version 1.0.1

--- src/main/java/com/mycompany/hidalgocarlos/TareaCompuesta.java
+++ src/main/java/com/mycompany/hidalgocarlos/TareaCompuesta.java
@@ -4,8 +4,12 @@ package com.mycompany.hidalgocarlos;
  *
  * @author sala4
  */
-public class TareaCompuesta {
+public class TareaCompuesta extends Tarea {
     
+    /**
+     *
+     */
+    @Override
     public void doEjecutar(){
         
     }
